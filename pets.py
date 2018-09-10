bc9befeb07a1f0940d3e77952e78bbd8839d6292
Antonio Marto <antoniomarto@Lenias-MacBook-Pro.local>
2018-09-10 23:24:01 +0800

    pets.py

---
+++ pets.py
@@ -0,0 +1,4 @@
+class Pet(object): # Pet is a class of the type `object` 
+    def __init__(self, name, species): # Class initialization: _init__ is method (special Python function) is called when a instance of a class is first created.
+    shelf.name = name
+    shelf.species = species
